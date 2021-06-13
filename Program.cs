dbc2e71a374cee182e0c982e02d32870e09fd076
Gabriela <aragao.gabi@gmail.com>
2021-06-13 15:45:29 -0300

    novas atualizações

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite o  primeiro número:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Escolha a operação (+ - x /): ");
+
+            char op =  char.Parse(Console.ReadLine());
+            double resultado = 0;
+
+            switch(op){
+                default:
+                Console.WriteLine(); break;
+
+                case '+':
+                    resultado = num1 + num2;
+                    Console.WriteLine("O resultado da soma é: " + resultado);break;
+
+                case '-':
+                    resultado = num1 - num2;
+                    Console.WriteLine("O resultado da subtração é: "+ resultado);break;
+
+                case 'x': case'X': case'*':
+                    resultado = num1 * num2;
+                    Console.WriteLine("O resultado  da multiplicação é: "+ resultado);break;
+
+                 case '/':
+                 
+                 if(num2 == 0)
+                 {
+                     Console.WriteLine("Não é possível dividir por zero", resultado);
+
+                 }else{
+
+                    resultado = num1 / num2;
+                    Console.WriteLine("O resultado  da divisão é: ", resultado);
+                 }break;                    
+            }
         }
     }
 }
